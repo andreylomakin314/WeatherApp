cd81a2f08ee241dab9a464d1fb236d60af86370d
Andrey <102652716+andreylomakin314@users.noreply.github.com>
2022-07-18 17:41:39 +0300

    Update apps.py

--- weather/apps.py
+++ weather/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class WeatherConfig(AppConfig):
+    """Сохраняем метаданные в экземпляре"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'weather'
